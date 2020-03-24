b46e12d9a9a5b832347032ce4b458babcc0bc918
rathovarun <varunrathore1032@gmail.com>
2020-03-25 01:52:24 +0530

    Updates

---
+++ precache-manifest.d1f5ec696eb965f332d067ba3314c2a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11bb8750f688325cde15303b2d486742",
+    "url": "/audiovisual/index.html"
+  },
+  {
+    "revision": "0023af0f28ec2e0c531c",
+    "url": "/audiovisual/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5f66c702330e7b697833",
+    "url": "/audiovisual/static/js/2.93659701.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/audiovisual/static/js/2.93659701.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0023af0f28ec2e0c531c",
+    "url": "/audiovisual/static/js/main.bb42a1e4.chunk.js"
+  },
+  {
+    "revision": "068a8c9e89f4e7e9b7f6",
+    "url": "/audiovisual/static/js/runtime-main.3bc32635.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/audiovisual/static/media/logo.5d5d9eef.svg"
+  }
+]);
